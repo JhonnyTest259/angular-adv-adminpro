d8e8a61fd7cdc53b01be359763320e1142013b90
Jhonatan Alejandro Mejia Mendoza <jhobatrix@gmail.com>
2023-01-11 10:51:43 -0500

    Fin seccion 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,11 +5,11 @@ import { RouterModule } from '@angular/router';
 import { ComponentsModule } from '../components/components.module';
 import { SharedModule } from '../shared/shared.module';
 
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +17,10 @@ import { PagesComponent } from './pages.component';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   exports: [
+    AccountSettingsComponent,
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
